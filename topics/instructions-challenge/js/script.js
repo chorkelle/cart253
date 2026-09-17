c2bee9215fdb514ffc34a2dd9ed4657445085915
chorkelle <charlottebriwalsh@gmail.com>
2026-09-17 14:59:57 -0400

    Started the instructions challenge

---
+++ topics/instructions-challenge/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Instructions Challenge
+ * Charlotte Walsh
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas
+*/
+function setup() {
+    createCanvas(1920, 1080)
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
